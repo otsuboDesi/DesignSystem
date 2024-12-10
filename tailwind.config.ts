cd8754f744428510816e08fbdd12b60251964bd9
otsuboDesi <otsubodesignare@gmail.com>
2024-12-10 14:22:08 +0900

    Add: Noto Sans JP

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,7 +6,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Roboto', 'Noto Sans', 'sans-serif'],
+        sans: ['Noto Sans JP', 'sans-serif'],
       },
       fontSize: {
         h1: '83px',
